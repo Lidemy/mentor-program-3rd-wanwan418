a0cb60a14dae8e2b652cb2af39d57f4f97dc1a43
julia <skylife21666@gmail.com>
2019-04-27 14:53:45 +0800

    finish week2 hw

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-  return str;
+  let strReverse = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    strReverse += str[i];
+  }
+  console.log(strReverse);
 }
 
 reverse('hello');
