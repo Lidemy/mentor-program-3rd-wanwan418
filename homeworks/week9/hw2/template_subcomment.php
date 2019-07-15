7e472d07594b588bbd16d14aa07919679a9ebe0a
julia <skylife21666@gmail.com>
2019-07-15 22:57:28 +0800

    fixed sync error

--- homeworks/week9/hw2/template_subcomment.php
+++
@@ -1,7 +0,0 @@
-<div class="commentSub">
-  <div class="commentHeader">
-    <div class="commentAuthor"><?php echo $row['nickname'] ?></div>
-    <div class="commentTime"><?php echo $row['created_at'] ?></div> 
-  </div>
-  <div class="commentContent"><?php echo $sub_comment['content'] ?></div>
-</div>
